bfd5a62224ccaf7147d3c29f71a778df8dde8667
kanchan.m <mkanchan579@gmail.com>
2025-03-27 18:15:05 +0530

    26th March - Pushing code

--- src/Task/Math_Functions.java
+++ src/Task/Math_Functions.java
@@ -3,19 +3,11 @@ import java.lang.Math;
 public class Math_Functions {
     public static void main(String[] args) {
         {
-            //equation -> ∛(x^2 + y^2 + |z|)
+            //equation -> ∛(x^2 + y^2 - |z|)
             int x = 10;
             int y = 10;
-            int z = -10;
-            int a = Math.abs(z);
-            double b = Math.pow(x, 2);
-            double c = Math.pow(y, 2);
-            // double d = a + b + c;
-            double e = Math.cbrt(a+b+c);
-
-
-            System.out.println("The result of the expression ∛(x^2 + y^2 + |z|) is : " + e);
-
+            int z = 10;
+            System.out.println("The result of the expression ∛(x^2 + y^2 - |z|) is : " + Math.cbrt(Math.pow(x,2) + Math.pow(y,2) - Math.abs(z)));
         }
     }
 }
